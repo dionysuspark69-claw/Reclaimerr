51aa10c4ee3eb0d368580475999a66bee712f1c3
jessielw <jessielw4049@gmail.com>
2026-02-15 23:44:35 -0500

    feat: place holder

---
+++ frontend/src/routes/Movies.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<div class="p-8">
+  <div class="max-w-7xl mx-auto">
+    <div class="mb-8">
+      <h1 class="text-3xl font-bold text-gray-100 mb-2">Movies</h1>
+      <p class="text-gray-400">Place holder</p>
+    </div>
+  </div>
+</div>
